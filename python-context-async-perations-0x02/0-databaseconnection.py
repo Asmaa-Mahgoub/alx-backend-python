40d34afb2dcddf8f23720c9b9664a597fd285e39
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-11-09 15:48:37 +0200

    Add init method to the class as not to hard code the name of database

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,20 +1,29 @@
 import sqlite3
 class DatabaseConnection:
+    def __init__(self, db_name):
+        # The __init__ method runs automatically when creating the object
+        # It stores the database name but does not connect yet
+        self.db_name = db_name
+        self.conn = None   # placeholder for the connection object
+        print(f"DatabaseConnection object created for {self.db_name}")
+
+
     def __enter__(self):
-        
-        self.conn=sqlite3.connect("users.db")
+        self.conn=sqlite3.connect(self.db_name)
         print("Database connection opened.")
         # Return the connection so it can be used inside the 'with' block
         return self.conn
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+
+    def __exit__(self, exc_type, exc_value, traceback):
         if self.conn:
             self.conn.close()
-            print("Database connection closed.")
+            print(f"Database connection to '{self.db_name}' closed.")
         # Return False so any exceptions are not suppressed
         return False
 
-with DatabaseConnection() as conn:
+
+with DatabaseConnection("users.db") as conn:
     cursor=conn.cursor()
     cursor.execute("SELECT * FROM users")
     results=cursor.fetchall
