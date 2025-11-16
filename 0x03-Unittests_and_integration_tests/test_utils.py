99306daa5923317390cfd7b309d7f35e47ae693a
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-11-16 18:46:44 +0200

    Fine tune the code to match autochecker

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,12 +16,13 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), 'a'),
         ({"a": 1}, ("a", "b"), 'b')
     ])
-        def test_access_nested_map_exception(self, nested_map, path, expected):
-            """ Test that a KeyError is raised for the respective inputs """
+        def test_access_nested_map_exception(self, nested_map, path):
+            """Test that KeyError is raised for invalid paths."""
             with self.assertRaises(KeyError) as e:
                 access_nested_map(nested_map, path)
-            self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
-
+            self.assertEqual(str(e.exception), repr(path[-1]))    
+       
+    
 if __name__=="__main__":
     unittest.main()
 
